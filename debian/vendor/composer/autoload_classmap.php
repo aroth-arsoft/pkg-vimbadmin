31c7cc4dcbb6fd2d097b901a193f7248bca6b56c
Andreas Roth <aroth@arsoft-online.com>
2015-01-03 18:45:38 +0100

    use existing packages and ship with missing vendor extensions

---
+++ debian/vendor/composer/autoload_classmap.php
@@ -0,0 +1,11 @@
+<?php
+
+$smarty_dir = '/usr/share/php/smarty3';
+
+return array(
+    'Smarty' => $smarty_dir . '/Smarty.class.php',
+    'SmartyBC' => $smarty_dir . '/SmartyBC.class.php',
+    'SmartyCompilerException' => $smarty_dir . '/Smarty.class.php',
+    'SmartyException' => $smarty_dir . '/Smarty.class.php',
+    'Smarty_Security' => $smarty_dir . '/sysplugins/smarty_security.php',
+);
