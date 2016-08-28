480b2f545562e1b9014eba666b377e90d6ce3651
Andreas Roth <aroth@arsoft-online.com>
2016-08-28 11:45:48 +0200

    fixed syntax error in cron tab

--- debian/vimbadmin.cron.d
+++ debian/vimbadmin.cron.d
@@ -1,8 +1,5 @@
-SHELL=/bin/bash
-MAILTO=root
+15 3    * * *   root    /usr/share/vimbadmin/bin/vimbtool.php -a mailbox.cli-get-sizes
 
-15 3    * * *   /usr/share/vimbadmin/bin/vimbtool.php -a mailbox.cli-get-sizes
-
-*/5 *   * * *   /usr/share/vimbadmin/bin/vimbtool.php -a archive.cli-archive-pendings
-*/5 *   * * *   /usr/share/vimbadmin/bin/vimbtool.php -a archive.cli-delete-pendings
-*/5 *   * * *   /usr/share/vimbadmin/bin/vimbtool.php -a archive.cli-restore-pendings
+*/5 *   * * *   root    /usr/share/vimbadmin/bin/vimbtool.php -a archive.cli-archive-pendings
+*/5 *   * * *   root    /usr/share/vimbadmin/bin/vimbtool.php -a archive.cli-delete-pendings
+*/5 *   * * *   root    /usr/share/vimbadmin/bin/vimbtool.php -a archive.cli-restore-pendings
